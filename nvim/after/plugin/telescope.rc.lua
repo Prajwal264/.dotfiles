f88f25c9b8fef01cb4deb8e5665bc1d0f023e209
Prajwal264 <prajwalp264@gmail.com>
2022-10-29 18:30:47 +0530

    adding git hotkeys to telescope

--- nvim/after/plugin/telescope.rc.lua
+++ nvim/after/plugin/telescope.rc.lua
@@ -44,10 +44,21 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>ff',
   '<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true})<cr>',
   opts)
-vim.keymap.set('n', '<leader>fc',
+vim.keymap.set('n', '<leader>gc',
   '<cmd>lua require("telescope.builtin").git_commits({ no_ignore = false, hidden = true})<cr>',
   opts)
+vim.keymap.set('n', '<leader>fb',
+  '<cmd>lua require("telescope.builtin").file_browser({ no_ignore = false, hidden = true})<cr>',
+  opts)
+vim.keymap.set('n', '<leader>gb',
+  '<cmd>lua require("telescope.builtin").git_branches({ no_ignore = false, hidden = true})<cr>',
+  opts)
+vim.keymap.set('n', '<leader>gs',
+  '<cmd>lua require("telescope.builtin").git_status({ no_ignore = false, hidden = true})<cr>',
+  opts)
 vim.keymap.set('n', '<leader>fw', '<cmd>lua require("telescope.builtin").live_grep()<cr>',
+  opts)
+vim.keymap.set('n', '<leader>jl', '<cmd>lua require("telescope.builtin").jumplist()<cr>',
   opts)
 vim.keymap.set('n', '////', '<cmd>lua require("telescope.builtin").buffers()<cr>',
   opts)
