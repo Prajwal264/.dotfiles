8ee48a8ca656f7ad1da9c7249afc46a54414319b
Prajwal <prajwal@192.168.1.4>
2024-02-23 22:45:40 +0530

    feat: vs code support for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,43 @@
 if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
 
-for _, source in ipairs {
-  "astronvim.bootstrap",
-  "astronvim.options",
-  "astronvim.lazy",
-  "astronvim.autocmds",
-  "astronvim.mappings",
-} do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
-end
-
-if astronvim.default_colorscheme then
-  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
-    require("astronvim.utils").notify(
-      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
-      vim.log.levels.ERROR
-    )
+if vim.g.vscode then
+  -- VSCode extension
+  for _, source in ipairs {
+    "astronvim.bootstrap",
+    "astronvim.options",
+    "vscode.mappings"
+  } do
+    local status_ok, fault = pcall(require, source)
+    if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
   end
-end
+else
+  -- ordinary Neovim
+  for _, source in ipairs {
+    "astronvim.bootstrap",
+    "astronvim.options",
+    "astronvim.lazy",
+    "astronvim.autocmds",
+    "astronvim.mappings",
+  } do
+    local status_ok, fault = pcall(require, source)
+    if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+  end
 
-vim.api.nvim_exec(
-  [[
-let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
-]] ,
-  false
-)
-vim.opt.fixendofline = false
-require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+  if astronvim.default_colorscheme then
+    if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+      require("astronvim.utils").notify(
+        ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+        vim.log.levels.ERROR
+      )
+    end
+  end
+
+  vim.api.nvim_exec(
+    [[
+  let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+  ]] ,
+    false
+  )
+  vim.opt.fixendofline = false
+  require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+end 
---
+++ nvim/lua/vscode/mappings.lua
@@ -0,0 +1,43 @@
+local utils = require "astronvim.utils"
+local maps = require("astronvim.utils").empty_map_table()
+local vscode = require 'vscode-neovim'
+
+vim.api.mapleader = " ";
+vim.notify = vscode.notify
+
+maps.n["<leader>s"] = { "<cmd>Write<cr>", desc = "Save File" }
+maps.n["<leader>e"] = { function() vscode.call("workbench.action.focusSideBar") end, desc = "Focus sidebar" }
+maps.n["<leader>ff"] = { function() vscode.call("workbench.action.quickOpen") end, desc = "Find Files" }
+maps.n["<leader>fw"] = { function() vscode.call("workbench.action.findInFiles") end, desc = "Grep in Files" }
+-- Harpoon
+maps.n["<leader>hm"] = { 
+  function() 
+    vscode.call("vscode-harpoon.addEditor");
+    vim.notify('Added file to harpoon')
+  end, 
+  desc = "Add file to harpoon" 
+}
+maps.n["<leader>hh"] = { function() vscode.call("vscode-harpoon.editorQuickPick") end, desc = "Pick from harpoon" }
+maps.n["<leader>h1"] = { function() vscode.call("vscode-harpoon.gotoEditor1") end, desc = "Go to harpoon file 1" }
+maps.n["<leader>h2"] = { function() vscode.call("vscode-harpoon.gotoEditor2") end, desc = "Go to harpoon file 2" }
+maps.n["<leader>h3"] = { function() vscode.call("vscode-harpoon.gotoEditor1") end, desc = "Go to harpoon file 3" }
+maps.n["<leader>h4"] = { function() vscode.call("vscode-harpoon.gotoEditor1") end, desc = "Go to harpoon file 4" }
+maps.n["<leader>gg"] = { function() vscode.call('lazygit.openLazygit') end , desc = "Open Lazygit" }
+maps.n["]b"] = { function() vscode.call('workbench.action.nextEditor') end , desc = "Next Buffer" }
+maps.n["[b"] = { function() vscode.call('workbench.action.previousEditor') end , desc = "Previous Buffer" }
+maps.n["<leader>/"] = { 
+  function()
+    vscode.action("editor.action.commentLine");
+  end, 
+  desc = "Toggle Comment" 
+}
+maps.v["<leader>/"] = { 
+  function()
+    vscode.action("editor.action.commentLine");
+  end, 
+  desc = "Toggle Comment" 
+}
+maps.v["<S-Tab>"] = { "<gv", desc = "Unindent line" }
+maps.v["<Tab>"] = { ">gv", desc = "Indent line" }
+
+utils.set_mappings(astronvim.user_opts("mappings", maps));
