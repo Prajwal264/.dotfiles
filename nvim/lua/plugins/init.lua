d8470118cb1f7ea288d95f8863f64797fe97ee9d
Prajwal P <prajwalp264@gmail.com>
2025-08-15 11:34:47 +0530

    feat: removing avante.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -78,42 +78,6 @@ return {
       require("telescope").load_extension('harpoon')
     end,
   },
-  {
-    "yetone/avante.nvim",
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
-    build = function()
-      -- conditionally use the correct build system for the current OS
-      if vim.fn.has("win32") == 1 then
-        return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-      else
-        return "make"
-      end
-    end,
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
-    opts = {
-      provider = "claude",
-      providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-sonnet-4-20250514",
-          timeout = 30000, -- Timeout in milliseconds
-            extra_request_body = {
-              temperature = 0.75,
-              max_tokens = 20480,
-            },
-        },
-      },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-  }
-
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
